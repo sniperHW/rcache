c94677f722487275d83587a7a025f3db1f2225f4
sniperhw <sniperhw@163.com>
2025-11-28 15:25:56 +0800

    rename

---
+++ rcache_test.go
@@ -0,0 +1,284 @@
+package rcache
+
+//go test -run=Get
+//go tool cover -html=coverage.out
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	//"sync"
+	"testing"
+	//"time"
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
+	redis "github.com/redis/go-redis/v9"
+)
+
+func TestRedisGet(t *testing.T) {
+	cli := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+
+	cli.FlushAll(context.TODO()).Result()
+
+	r, ver, err := RedisGet(context.Background(), cli, "hello")
+
+	fmt.Println(r, ver, err)
+
+	cli.Eval(context.Background(), "redis.call('hmset','hello','version',1,'value','hello')", []string{})
+
+	r, ver, err = RedisGet(context.Background(), cli, "hello")
+
+	fmt.Println(r, ver, err)
+
+	fmt.Println(cli.TTL(context.Background(), "hello"))
+
+	cli.Eval(context.Background(), "redis.call('del','hello')", []string{})
+
+	r, ver, err = RedisGet(context.Background(), cli, "hello")
+
+	fmt.Println(r, ver, err)
+}
+
+func TestRedisLoadGet(t *testing.T) {
+	cli := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+
+	cli.FlushAll(context.TODO()).Result()
+
+	r, v, err := RedisLoadGet(context.Background(), cli, "hello", 0, "")
+
+	fmt.Println("loadGet", r, v, err)
+
+	fmt.Println(cli.TTL(context.Background(), "hello"))
+
+	fmt.Println(cli.Del(context.Background(), "hello").Result())
+
+	r, v, err = RedisLoadGet(context.Background(), cli, "hello", 1, "world")
+
+	fmt.Println("loadGet again", r, v, err)
+
+	fmt.Println(cli.TTL(context.Background(), "hello"))
+
+	fmt.Println(cli.Del(context.Background(), "hello").Result())
+}
+
+func TestRedisLoadSet(t *testing.T) {
+	cli := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+	cli.FlushAll(context.TODO()).Result()
+
+	err := RedisLoadSet(context.Background(), cli, "hello", 2, "world")
+
+	fmt.Println(err)
+
+	fmt.Println(cli.TTL(context.Background(), "hello"))
+
+	time.Sleep(time.Second * 2)
+
+	err = RedisLoadSet(context.Background(), cli, "hello", 1, "world")
+
+	fmt.Println(err)
+
+	fmt.Println(cli.TTL(context.Background(), "hello"))
+
+	err = RedisLoadSet(context.Background(), cli, "hello", 3, "world")
+
+	fmt.Println(err)
+
+	fmt.Println(cli.TTL(context.Background(), "hello"))
+
+	fmt.Println(cli.Del(context.Background(), "hello").Result())
+}
+
+func TestRedisSet(t *testing.T) {
+	cli := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+
+	cli.FlushAll(context.Background()).Result()
+
+	ver, err := RedisSet(context.Background(), cli, "hello", "world")
+	fmt.Println(err, ver)
+
+	RedisLoadSet(context.Background(), cli, "hello", 1, "world")
+
+	fmt.Println(RedisGet(context.Background(), cli, "hello"))
+
+	ver, err = RedisSet(context.Background(), cli, "hello", "world2")
+	fmt.Println(err, ver)
+
+	fmt.Println(RedisGet(context.Background(), cli, "hello"))
+
+	//版本不一致，更新失败
+	ver, err = RedisSetWithVersion(context.Background(), cli, "hello", "world3", 1)
+	fmt.Println(err, ver)
+
+	ver, err = RedisSetWithVersion(context.Background(), cli, "hello", "world3", ver)
+	fmt.Println(err, ver)
+
+	fmt.Println(RedisGet(context.Background(), cli, "hello"))
+
+}
+
+/*
+func TestPGUpdate(t *testing.T) {
+	dbc, err := sqlx.Open("postgres", "host=localhost port=15432 dbname=test user=postgres password=802802 sslmode=disable")
+
+	version, err := insertUpdateRowPgsql(context.TODO(), dbc, "hello", "world")
+
+	fmt.Println(version, err)
+}
+
+func TestCacheGet(t *testing.T) {
+	dbc, err := sqlx.Open("postgres", "host=localhost port=15432 dbname=test user=postgres password=802802 sslmode=disable")
+	cli := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+
+	proxy := dataproxy{
+		rediscli: cli,
+		dbc:      dbc,
+	}
+
+	fmt.Println(cli.Del("hello").Result())
+	dbc.Exec("delete from kv where key = 'hello';")
+
+	value, _, err := proxy.Get(context.TODO(), "hello")
+
+	fmt.Println(value, err)
+
+	fmt.Println(cli.HGet("hello", "version").Int())
+
+	fmt.Println(cli.TTL("hello"))
+
+}
+
+func TestCacheSet(t *testing.T) {
+	dbc, err := sqlx.Open("postgres", "host=localhost port=15432 dbname=test user=postgres password=802802 sslmode=disable")
+	cli := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+
+	proxy := dataproxy{
+		rediscli: cli,
+		dbc:      dbc,
+	}
+
+	cli.Del("hello").Result()
+	dbc.Exec("delete from kv where key = 'hello';")
+
+	_, err = proxy.Set(context.TODO(), "hello", "world")
+
+	fmt.Println("set", err)
+
+	fmt.Println(cli.HGet("hello", "version").Int())
+
+	fmt.Println(cli.TTL("hello"))
+
+}
+
+func TestScan(t *testing.T) {
+	dbc, _ := sqlx.Open("postgres", "host=localhost port=15432 dbname=test user=postgres password=802802 sslmode=disable")
+
+	cli := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+	scancli := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		DB:   1,
+	})
+
+	proxy := dataproxy{
+		rediscli: cli,
+		scancli:  scancli,
+		dbc:      dbc,
+	}
+
+	for i := 0; i < 100; i++ {
+		str := fmt.Sprintf("key:%d", i)
+		_, err := proxy.Set(context.TODO(), str, str)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	err := proxy.SyncDirtyToDB()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func TestRCache(t *testing.T) {
+	dbc, _ := sqlx.Open("postgres", "host=localhost port=15432 dbname=test user=postgres password=802802 sslmode=disable")
+
+	cli := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+	scancli := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		DB:   1,
+	})
+
+	proxy := dataproxy{
+		rediscli: cli,
+		scancli:  scancli,
+		dbc:      dbc,
+	}
+
+	cacheTimeout = 5
+
+	cli.Del("testkey").Result()
+	dbc.Exec("delete from kv where key = 'testkey';")
+
+	value, _, err := proxy.Get(context.TODO(), "testkey")
+
+	fmt.Println(value)
+
+	assert.Equal(t, "err_not_exist", err.Error())
+
+	//直接从redis获取
+	version, _ := cli.HGet("testkey", "version").Result()
+	assert.Equal(t, "0", version)
+
+	//等待timeout
+	fmt.Println("wait for timeout...")
+	time.Sleep(time.Second * 5)
+	_, err = cli.HGet("testkey", "version").Result()
+	assert.Equal(t, err.Error(), "redis: nil")
+
+	_, err = proxy.Set(context.TODO(), "testkey", "testvalue")
+
+	assert.Nil(t, err)
+	version, _ = cli.HGet("testkey", "version").Result()
+	assert.Equal(t, "1", version)
+
+	fmt.Println(cli.TTL("testkey"))
+
+	_, err = proxy.Set(context.TODO(), "testkey", "testvalue2")
+
+	assert.Nil(t, err)
+	version, _ = cli.HGet("testkey", "version").Result()
+	assert.Equal(t, "2", version)
+
+	fmt.Println(cli.TTL("testkey"))
+
+	value, _, _ = proxy.Get(context.TODO(), "testkey")
+	assert.Equal(t, value, "testvalue2")
+	fmt.Println(cli.TTL("testkey"))
+
+	proxy.SyncDirtyToDB()
+
+	fmt.Println(cli.TTL("testkey"))
+
+	fmt.Println("wait for timeout...")
+	time.Sleep(time.Second * 5)
+	_, err = cli.HGet("testkey", "version").Result()
+	assert.Equal(t, err.Error(), "redis: nil")
+
+}
+*/
